d8d6e77dc8b61fe8885102478b4c7ebf059dbb97
Jonatas Sallazar <jonatassallazar@gmail.com>
2021-08-01 17:30:15 -0300

    added auto dummy data

--- api/src/db/connection.js
+++ api/src/db/connection.js
@@ -1,15 +1,45 @@
 // connection.js
 const mongoose = require("mongoose");
+const Clientes = require("../models/Clientes.model")
+const faker = require("faker/locale/pt_BR");
+const moment = require("moment");
 
-const connection = "mongodb://mongo:27017/test-totvs-db";
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-const connectDb = () => {
-  return mongoose.connect(connection, {
+async function seedDB() {
+  const connectionUri = "mongodb://localhost:27017/test-totvs-db";
+  
+  await mongoose.connect(connectionUri, {
     useUnifiedTopology: true,
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
   });
-};
 
-module.exports = connectDb;
+  try {
+    await Clientes.deleteMany({});
+
+    let timeSeriesData = [];
+    for (let i = 0; i < 100; i++) {
+      const nome = faker.name.firstName() + " " + faker.name.lastName();
+      let newClient = {
+        dataVencimento: moment("2021-07-01").add(i, "day").valueOf(),
+        valor: randomIntFromInterval(10000, 50000),
+        nome,
+      };
+
+      timeSeriesData.push(newClient);
+    }
+    Clientes.insertMany(timeSeriesData)
+    console.log("Database seeded! :)");
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
+seedDB();
+
+module.exports = seedDB;
